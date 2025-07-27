ffe4c68a215786e1fc50f14e76cd933b7bc94f05
ronbourges <bourgesron@mail.com>
2025-07-27 18:48:47 -0300

    Primeiras mudanças css selector

---
+++ cypress/e2e/login.spec.cy.js
@@ -0,0 +1,18 @@
+describe('Orange HRM tests', () => {
+  it('Login-success', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('Admin')
+    cy.get("[name='password']").type('admin123')
+   cy.get("[type='submit']").click()
+   cy.location('pathname').should('equal','/web/index.php/dashboard/index') 
+   cy.get('.oxd-topbar-header-breadcrumb-module').contains('Dashboard')
+  
+  })
+  it('Login-fail', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.get("[name='username']").type('test')
+    cy.get("[name='password']").type('test')
+   cy.get("[type='submit']").click()
+   cy.get("[role='alert']")
+  })
+})
