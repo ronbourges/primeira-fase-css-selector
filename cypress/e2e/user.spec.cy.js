509cd90132a78239264c846c9b020adda69c71e2
ronbourges <bourgesron@mail.com>
2025-08-11 22:47:54 -0300

    selectors list update

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -7,18 +7,27 @@ describe('Orange HRM tests', () => {
     loginButton:  "[type='submit']",
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb-module',
     dashboarGrid: ".orangehrm-dashboard-grid",
-    wrongCredentionAlert: "[role='alert']"
+    wrongCredentionAlert: "[role='alert']",
+    myInfoButton: '[href="/web/index.php/pim/viewMyDetails"]',
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    idField: ".oxd-input--active",
+    dataExpireField: ".oxd-input--active"
+
 
    
   }
-    it('Login-success', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    it.only('User Info Update-success', () => {
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username)
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)
    cy.get(selectorsList.loginButton).click()
    cy.location('pathname').should('equal','/web/index.php/dashboard/index') 
    cy.get(selectorsList.dashboarGrid)
-  
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.idField).eq(4).type('muser')
+    cy.get(selectorsList.dataExpireField).eq(7).type(2025)
+    
   })
   it('Login-fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
